0c216afc645d798daffcd71abb20a25fb3b3cb12
gerardo gandia <gerardogandia@gmail.com>
2014-08-07 12:05:27 +0200

    pruebas de menus

--- app/js/main.js
+++ app/js/main.js
@@ -13,4 +13,34 @@ menuObj.addEventListener("click", function(){
 });
 
 
+var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+        menuRight = document.getElementById( 'cbp-spmenu-s2' ),
+        menuTop = document.getElementById( 'cbp-spmenu-s3' ),
+        menuBottom = document.getElementById( 'cbp-spmenu-s4' ),
+        showLeft = document.getElementById( 'showLeft' ),
+        showRight = document.getElementById( 'showRight' ),
+        showTop = document.getElementById( 'showTop' ),
+        showBottom = document.getElementById( 'showBottom' ),
+        showLeftPush = document.getElementById( 'showLeftPush' ),
+        showRightPush = document.getElementById( 'showRightPush' ),
+        body = document.body;
  
+
+showRight.onclick = function() {
+    classie.toggle( this, 'active' );
+    classie.toggle( menuRight, 'cbp-spmenu-open' );
+    disableOther( 'showRight' );
+};
+
+
+
+ 
+function disableOther( button ) {
+   
+    if( button !== 'showRight' ) {
+        classie.toggle( showRight, 'disabled' );
+    }
+   
+  
+   
+}
