ad816a9fb6f482a18eacbc16e46ec50d8ca9056e
gerardo gandia <gerardogandia@gmail.com>
2014-08-07 17:55:33 +0200

    Menú lateral casi acabado
    Menú lateral casi acabado

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,31 @@
-var menuObj = document.getElementsByClassName("menu-mobil")[0];
-
-menuObj.addEventListener("click", function(){
-	var menu  = document.getElementById("menu_principal");
-	var menuObj = document.getElementsByClassName("img-menu-mobil")[0];
-	if(menu.style.left!="0px"){
-		menu.style.left="0px";
-		menuObj.style.left="70%";
-	}else{
-		menu.style.left="-70%";
-		menuObj.style.left="0%";
-	}
-});
+//var menuObj = document.getElementsByClassName("menu-mobil")[0];
+//
+//menuObj.addEventListener("click", function(){
+//	var menu  = document.getElementById("menu_principal");
+//	var menuObj = document.getElementsByClassName("img-menu-mobil")[0];
+//	if(menu.style.left!="0px"){
+//		menu.style.left="0px";
+//		menuObj.style.left="70%";
+//	}else{
+//		menu.style.left="-70%";
+//		menuObj.style.left="0%";
+//	}
+//});
 
 /*Menú movil */
 var    menuRight = document.getElementById( 'cbp-spmenu-s2' ),
         body = document.body;
+
 showRight.onclick = function() {
     classie.toggle( this, 'active' );
     classie.toggle( menuRight, 'cbp-spmenu-open' );
      
 };
-
+closeMenu.onclick = function() {
+    classie.toggle( this, 'active' );
+    classie.toggle( menuRight, 'cbp-spmenu-open' );
+     
+};
 
 
  
